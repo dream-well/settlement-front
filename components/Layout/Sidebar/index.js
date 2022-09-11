a4d796dd82496933cbd18a99414edaba82245f7f
dream-well <dreamwell506@outlook.com>
2022-09-11 15:42:29 +0800

    fix title

--- components/Layout/Sidebar/index.js
+++ components/Layout/Sidebar/index.js
@@ -14,7 +14,7 @@ export default function Sidebar() {
     return (
         <div className={cn('flex flex-col w-[242px] shadow-[0_2px_150px_0px_rgba(90,97,105,0.1)] z-10 transition-all duration-500', menuState == 'hidden' && '-ml-[242px] opacity-0')}>
             <nav className='h-[60px] w-full text-center leading-[60px] border-b border-[#e1e5eb] text-[#222] font-medium mb-[2px]'>
-                <div className='flex items-center justify-center cursor-pointer text-[20px]'>
+                <div className='flex items-center cursor-pointer text-[20px] ml-[26px]'>
                     <i className='material-icons'>attach_money</i>
                     <span className='ml-[4px]'>
                         <span className='text-[#f39c2b]'>Digi</span>
