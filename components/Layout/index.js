7c622e7f168a7f5869b580adc236d39c0da431e1
dream-well <dreamwell506@outlook.com>
2022-09-23 04:03:06 +0800

    add signature

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -14,8 +14,8 @@ function Layout({children, title}) {
                 <div className='bg-[#f5f6f8] flex-grow px-4'>
                     <div className="min-h-full">
                         <div className="flex justify-between">
-                            <h3 className='py-4 text-[25px] font-bold text-[#1e2f65]'>{title}</h3>
-                            <div className="flex items-end">
+                            <h3 className='py-8 pl-8 text-[25px] font-bold text-[#1e2f65]'>{title}</h3>
+                            <div className="flex items-center">
                                 <span className="font-medium text-[12px]">Maintenance Mode:</span>
                                 {
                                     maintenanceState ?
