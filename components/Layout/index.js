0fd340d16cd9bd47e4f686be141df32f3124d5f5
PassionFriend <divas1635@gmail.com>
2022-09-22 09:03:19 +0900

    update swap

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,25 +7,23 @@ import { selectmaintenanceState } from "store/slices/maintenanceSlice";
 function Layout({children, title}) {
     const maintenanceState = useSelector(selectmaintenanceState);
     return (
-        <div className='flex min-h-screen w-full'>
+        <div className='flex w-full'>
             <Sidebar />
-            <div className='flex flex-col w-full'>
+            <div className='flex flex-col w-full min-h-screen'> 
                 <Header />
-                <div className='bg-[#f5f6f8] flex-grow px-4'>
-                    <div className="min-h-full">
-                        <div className="flex justify-between">
-                            <h3 className='py-4 text-[25px] font-bold text-[#1e2f65]'>{title}</h3>
-                            <div className="flex items-center py-4">
-                                <span className="font-medium text-[12px]">Maintenance Mode:</span>
-                                {
-                                    maintenanceState ?
-                                    <span className="bg-[#dceacb] text-[#b5c918] text-[10px] rounded-sm mx-1 px-1">Yes</span> : 
-                                    <span className="bg-[#fad4d4] text-[#e82c5d] text-[10px] rounded-sm mx-1 px-1">No</span>    
-                                }
-                            </div>
+                <div className='bg-[#f5f6f8] flex-grow px-4 flex flex-col'>
+                    <div className="flex justify-between">
+                        <h3 className='py-4 text-[25px] font-bold text-[#1e2f65]'>{title}</h3>
+                        <div className="flex items-center py-4">
+                            <span className="font-medium text-[12px]">Maintenance Mode:</span>
+                            {
+                                maintenanceState ?
+                                <span className="bg-[#dceacb] text-[#b5c918] text-[10px] rounded-sm mx-1 px-1">Yes</span> : 
+                                <span className="bg-[#fad4d4] text-[#e82c5d] text-[10px] rounded-sm mx-1 px-1">No</span>    
+                            }
                         </div>
-                        {children}
                     </div>
+                    {children}
                     <Footer/>
                 </div>
             </div>
